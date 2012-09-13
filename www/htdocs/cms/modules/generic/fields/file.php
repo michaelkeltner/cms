dcb75fd89aec59240a484e7f6f5007fb445c11d9
Michael Keltner <Michael Keltner@FB19HN1.ahp.academichealthplans.com>
2012-09-13 16:44:57 -0500

    varaible was being overwritten when the options.php file was included.  Changed the name of the variable locally

--- www/htdocs/cms/modules/generic/fields/file.php
+++ www/htdocs/cms/modules/generic/fields/file.php
@@ -1,5 +1,6 @@
 <?php 
-    $aOptions = $oAsset->getAll(' WHERE `type` = "doc"');
+    $aAssetOptions = $oAsset->getAll(' WHERE `type` = "doc"');
+
     $aFieldOptions = unserialize($oPropery->options);
     if ($aFieldOptions['multiple']){
         $sMultiple = ' multiple="multiple"';
@@ -8,16 +9,17 @@
         $sMultiple = '';
         $sFirstEntry = '-- Select --';
     }
-    $aId = unserialize($sValue); 
+    $aId = @unserialize($sValue); 
      if (!is_array($aId)){
         $aId = array();
     }
 ?>
 <p class="name<?php include('options.php')?>">
-    <?php if (count($aOptions)):?>
+    <?php if (count($aAssetOptions)):?>
     <select name="<?php echo $sFieldName?>[]"<?php echo $sMultiple?>>
         <option value="0"><?php echo $sFirstEntry?></option>
-       <?php foreach($aOptions as $oOption):?>
+        
+       <?php foreach($aAssetOptions as $oOption):?>
         <option value="<?php echo $oOption->id?>"<?php if(in_array($oOption->id, $aId)):?> selected="selected"<?php endif;?>><?php echo $oOption->display_name?></option>
        <?php endforeach;?>
     </select><label for="<?php echo $sFieldName?>"><?php echo $sDisplay?></label>
