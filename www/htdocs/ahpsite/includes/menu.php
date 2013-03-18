3f2f61e5f84761099a7c5ae66f0aad2d19ec6624
Michael Keltner <Michael Keltner@FB19HN1.ahp.academichealthplans.com>
2013-03-18 11:35:55 -0500

    config: updated the file decacher stamp menu: added staff link ahpsite: updated the width percentage of the ahpmenu ul

--- www/htdocs/ahpsite/includes/menu.php
+++ www/htdocs/ahpsite/includes/menu.php
@@ -5,6 +5,7 @@
     <ul>
         <li><a href="<?php echo SITE_URL?>schoollist"<?php if (isInUrl('schoollist')):?> class="current"<?php endif;?>>School List</a></li>
         <li><a href="<?php echo SITE_URL?>suggest"<?php if (isInUrl('suggest')):?> class="current"<?php endif;?>>E-Suggest</a></li>
+        <li><a href="<?php echo SITE_URL?>staff"<?php if (isInUrl('staff')):?> class="current"<?php endif;?>>Staff</a></li>
         <li><a href="<?php echo SITE_URL?>changes"<?php if (isInUrl('changes')):?> class="current"<?php endif;?>>Changes</a></li>
         <li><a href="<?php echo SITE_URL?>filetransfer"<?php if (isInUrl('filetransfer')):?> class="current"<?php endif;?>>File Transfer</a></li>
         <li><a href="<?php echo SITE_URL?>faq"<?php if (isInUrl('faq')):?> class="current"<?php endif;?>>FAQ</a></li>
